05a194247a2a98d912923a5047cfe8b89fe0537f
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2026-02-28 01:10:26 +0530

    Add assignment routes and controllers for fetching assignments

--- SqlTesterBackend/src/app.ts
+++ SqlTesterBackend/src/app.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser';
+import assignmentRoutes from './routes/assignmentRoutes'
+import actionRoutes from './routes/actionRoutes'
+import authRoutes from './routes/authRoutes'
 
 const app = express();
 
@@ -14,7 +17,11 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
-app.use((err: any, req: Request, res: Response ) => {
+app.use('/api/assignments', assignmentRoutes)
+app.use('/api/auth', authRoutes)
+app.use('/api/action', actionRoutes)
+
+app.use((err: any, req: Request, res: Response,next: any ) => {
     console.error(err);
     res.status(err.status || 500).json({
         message: err.message || 'Internal Server Error'
